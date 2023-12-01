babfdde7802eddc1feb3a1be86d9ed4e86b0f072
용광순 <gs.yong@hicomp.net>
2023-12-01 13:23:31 +0900

    배포 했습니다

--- web-src/src/components/admin/gallery/gallery/modal/GalleryManageModalMain.jsx
+++ web-src/src/components/admin/gallery/gallery/modal/GalleryManageModalMain.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import CountrySelect from "common/js/commonComponents/CountrySelect";
 import CurrencySelect from "common/js/commonComponents/CurrencySelect";
 import useConfirm from "hook/useConfirm";
@@ -27,17 +27,59 @@ const GalleryManageModalMain = (props) => {
 
     // select box options
     const [selectedCurrency, setSelectedCurrency] = useState("410");
-    const [postType, setPostType] = useState([]);
-    const [postStatus, setPostStatus] = useState([]);
-    const [paintType, setPaintType] = useState([]);
-    const [artType, setArtType] = useState([]);
-    const [participateType, setParticipateType] = useState([]);
+    const [postTypeOption, setPostTypeOption] = useState([]);
+    const [postStatusOption, setPostStatusOption] = useState([]);
+    const [paintTypeOption, setPaintTypeOption] = useState([]);
+    const [artTypeOption, setArtTypeOption] = useState([]);
+    const [participateTypeOption, setParticipateTypeOption] = useState([]);
 
     // refs
     const mainTitleKo = useRef(null);
     const mainTitleEn = useRef(null);
     const subTitleKo = useRef(null);
     const subTitleEn = useRef(null);
+    const postType = useRef(null);
+    const postStatus = useRef(null);
+    const paintType = useRef(null);
+    const artType = useRef(null);
+    const participateType = useRef(null);
+    const participateMemoKo = useRef(null);
+    const participateMemoEn = useRef(null);
+    const priceInfoKo = useRef(null);
+    const priceInfoEn = useRef(null);
+
+    useEffect(() => {
+        // SELECT 옵션 초기화
+        setSelectOption();
+    }, [countryBank, codes]);
+
+    const setSelectOption = () => {
+        // 게시타입
+        const postTypeArr = codes.filter((el) => el.code_type === "POST_TYPE");
+        setPostTypeOption(postTypeArr);
+
+        // 게시상태
+        const postStatusArr = codes.filter(
+            (el) => el.code_type === "POST_STATUS",
+        );
+        setPostStatusOption(postStatusArr);
+
+        // 재질타입
+        const paintTypeArr = codes.filter(
+            (el) => el.code_type === "PAINT_TYPE",
+        );
+        setPaintTypeOption(paintTypeArr);
+
+        // 미술타입
+        const artTypeArr = codes.filter((el) => el.code_type === "ART_TYPE");
+        setArtTypeOption(artTypeArr);
+
+        // 참여타입
+        const participateTypeArr = codes.filter(
+            (el) => el.code_type === "PARTICIPATE_TYPE",
+        );
+        setParticipateTypeOption(participateTypeArr);
+    };
 
     return (
         <>
@@ -53,7 +95,8 @@ const GalleryManageModalMain = (props) => {
                     <tbody>
                         <tr>
                             <th>
-                                작품명(국문) <span className="red">*</span>
+                                작품명 <span className="red">*</span> <br />
+                                (국문/영문)
                             </th>
                             <td>
                                 <input
@@ -61,7 +104,13 @@ const GalleryManageModalMain = (props) => {
                                     className="input wp100"
                                     ref={mainTitleKo}
                                     placeholder="작품명"
-                                />{" "}
+                                />
+                                <input
+                                    type="text"
+                                    className="input wp100"
+                                    ref={mainTitleEn}
+                                    placeholder="Title"
+                                />
                             </td>
                             <th>
                                 작품명(영문) <span className="red">*</span>
@@ -84,7 +133,7 @@ const GalleryManageModalMain = (props) => {
                                     className="input wp100"
                                     ref={subTitleKo}
                                     placeholder="부제목"
-                                />{" "}
+                                />
                             </td>
                             <th>부제목(영문)</th>
                             <td>
@@ -93,6 +142,129 @@ const GalleryManageModalMain = (props) => {
                                     className="input wp100"
                                     ref={subTitleEn}
                                     placeholder="Subtitle"
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>게시유형</th>
+                            <td>
+                                <select className="wp100" ref={postType}>
+                                    <option value="">- 선택 -</option>
+                                    {postTypeOption.length !== 0 &&
+                                        postTypeOption.map((item, idx) => (
+                                            <option
+                                                key={`postTypeOption_${idx}`}
+                                                value={item.code_key}
+                                            >{`${item.code_value_ko} (${item.code_value_en})`}</option>
+                                        ))}
+                                </select>
+                            </td>
+                            <th>게시상태</th>
+                            <td>
+                                <select className="wp100" ref={postStatus}>
+                                    <option value="">- 선택 -</option>
+                                    {postStatusOption.length !== 0 &&
+                                        postStatusOption.map((item, idx) => (
+                                            <option
+                                                key={`postTypeOption_${idx}`}
+                                                value={item.code_key}
+                                            >{`${item.code_value_ko} (${item.code_value_en})`}</option>
+                                        ))}
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>미술유형</th>
+                            <td>
+                                <select className="wp100" ref={artType}>
+                                    <option value="">- 선택 -</option>
+                                    {artTypeOption.length !== 0 &&
+                                        artTypeOption.map((item, idx) => (
+                                            <option
+                                                key={`postTypeOption_${idx}`}
+                                                value={item.code_key}
+                                            >{`${item.code_value_ko} (${item.code_value_en})`}</option>
+                                        ))}
+                                </select>
+                            </td>
+                            <th>재질유형</th>
+                            <td>
+                                <select className="wp100" ref={paintType}>
+                                    <option value="">- 선택 -</option>
+                                    {paintTypeOption.length !== 0 &&
+                                        paintTypeOption.map((item, idx) => (
+                                            <option
+                                                key={`postTypeOption_${idx}`}
+                                                value={item.code_key}
+                                            >{`${item.code_value_ko} (${item.code_value_en})`}</option>
+                                        ))}
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>참여유형</th>
+                            <td>
+                                <select className="wp100" ref={participateType}>
+                                    <option value="">- 선택 -</option>
+                                    {participateTypeOption.length !== 0 &&
+                                        participateTypeOption.map(
+                                            (item, idx) => (
+                                                <option
+                                                    key={`postTypeOption_${idx}`}
+                                                    value={item.code_key}
+                                                >{`${item.code_value_ko} (${item.code_value_en})`}</option>
+                                            ),
+                                        )}
+                                </select>
+                            </td>
+                            <th>
+                                참여자 메모
+                                <br />
+                                (국문/영문)
+                            </th>
+                            <td>
+                                <input
+                                    type="text"
+                                    className="input wp100"
+                                    ref={participateMemoKo}
+                                    placeholder="참여자 메모 (국문)"
+                                />
+                                <input
+                                    type="text"
+                                    className="input wp100"
+                                    ref={participateMemoEn}
+                                    placeholder="Participate Memo (English)"
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>통화코드</th>
+                            <td>
+                                <CurrencySelect
+                                    onChange={(e, value) =>
+                                        setSelectedCurrency(value)
+                                    }
+                                    defaultValue={selectedCurrency}
+                                    mode={"en"}
+                                />
+                            </td>
+                            <th>
+                                가격정보
+                                <br />
+                                (국문/영문)
+                            </th>
+                            <td>
+                                <input
+                                    type="text"
+                                    className="input wp100"
+                                    ref={priceInfoKo}
+                                    placeholder="가격정보 (국문)"
+                                />
+                                <input
+                                    type="text"
+                                    className="input wp100"
+                                    ref={priceInfoEn}
+                                    placeholder="Price Info (English)"
                                 />
                             </td>
                         </tr>
@@ -131,11 +303,6 @@ const GalleryManageModalMain = (props) => {
                     취소
                 </Link>
             </div>
-            {/*<CurrencySelect*/}
-            {/*    onChange={(e, value) => setSelectedCurrency(value)}*/}
-            {/*    defaultValue={selectedCurrency}*/}
-            {/*    mode={"full"}*/}
-            {/*/>*/}
         </>
     );
 };
